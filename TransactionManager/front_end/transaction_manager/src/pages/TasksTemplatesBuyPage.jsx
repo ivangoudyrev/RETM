eec05d6e5eb797fd35de5b33eb546280dde4b114
Ivan Goudyrev <ivan.goudyrev@gmail.com>
2023-09-09 14:51:35 -0400

    task template menu work

--- TransactionManager/front_end/transaction_manager/src/pages/TasksTemplatesBuyPage.jsx
+++ TransactionManager/front_end/transaction_manager/src/pages/TasksTemplatesBuyPage.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom"
 import { api } from "../utilities"
 import { useEffect, useState } from "react"
 import TaskTemplate from "../components/TaskTemplate";
+import Calendar from "../components/Calendar";
+import AllTasks from "../components/AllTasks";
 // import SubTaskTemplate from "../components/SubTaskTemplate";
 
 
@@ -97,68 +99,82 @@ export default function TasksTemplatesBuyPage(){
 
   return(
     <>
-      <div className="page_title_container">
-        <h1>"Buy" Task List Template</h1>
-      </div>
-      <div className="below_title_container">
-        <div className="left_side_container">
-          <div>
-            <div className="left_side_button_component">
-              <div
-                className="add_button_container"
-                style={{ display: showNewTaskButton ? "" : "none" }}>
-                <button onClick={newTaskHandle}>New Task</button>
+    <div className="container">
+      <div className="row mt-2 border">
+        <div className="col-lg-8 col-12">
+          <p className="h2">Tasks</p>
+          <div className="col-12">
+            <ul className="nav nav-tabs">
+              <li className="nav-item">
+                <Link to={`/tasks/master`} className="nav-link text-black border">
+                  <p className="h6">Master Task List</p>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={`/tasks/templates/buy`} className="nav-link active bg-secondary text-white" aria-current="page">
+                  <p className="h6">Task Templates</p>
+                </Link>
+              </li>
+            </ul>
+            <ul className="nav nav-tabs">
+              <li className="nav-item">
+                <Link to={`/tasks/templates/buy`} className="nav-link active bg-secondary text-white">
+                  <p className="h6">"Buy" Transaction Template</p>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={`/tasks/templates`} className="nav-link disabled border" aria-current="page">
+                  <p className="h6">"Sell" Transaction Templates</p>
+                </Link>
+              </li>
+            </ul>
+            <div className="d-grid gap-2 d-md-flex justify-content-md-end mt-2">
+              <button
+                className="btn btn-primary"
+                type="button"
+                style={{ display: showNewTaskButton ? "" : "none" }}
+                onClick={newTaskHandle}
+              > Add a Task</button>
+            </div>
+            <div 
+              className="card mt-2 bg-secondary pl-1"
+              style={{ display: showNewTaskBox ? "" : "none" }}>
+              <div className="toast-header d-flex justify-content-between align-items-center">
+                <input 
+                  type="text"
+                  id="task-input"
+                  className="form-control mx-1 mt-1 mb-1"
+                  placeholder="Add New Task Title"
+                  value={newTitle}
+                  onChange={(e) => setNewTitle(e.target.value)}/>
+              </div>
+              <div className="input-group mb-2">
+                <span className="input-group-text">Details</span>
+                <textarea 
+                  className="form-control"
+                  placeholder="Add a description of this task"
+                  aria-label="With textarea"
+                  value={newDetails}
+                  onChange={(e) => setNewDetails(e.target.value)}>
+                </textarea>
+              </div>
+              <div className="col-12">
+                <button 
+                  type="submit"
+                  className="btn btn-outline-secondary bg-white"
+                  onClick={newTaskSaveDiscardHandle}
+                >Cancel</button>
+                <button
+                  type="submit"
+                  className="btn btn-primary mx-2"
+                  onClick={addTemplateTask}
+                >Save</button>
               </div>
             </div>
-            <div className="left_side_component">
-              <div 
-                id="new_property_form_container" 
-                className="property_container"
-                style={{ display: showNewTaskBox ? "" : "none" }}>
-                <div className="property_info_container">
-                  <div className="task_title">
-                    <input 
-                      className="street_dynamic_input"
-                      placeholder="Task Title"
-                      size=""
-                      value={newTitle}
-                      onChange={(e) => setNewTitle(e.target.value)}
-                    />
-                  </div>
-                  <div>
-                    <textarea 
-                      className="task_details"
-                      name="" 
-                      id="" 
-                      cols="30" 
-                      rows="4"
-                      placeholder="Notes"
-                      value={newDetails}
-                      // disabled={!editMode}
-                      onChange={(e) => setNewDetails(e.target.value)}
-                    >  
-                    </textarea>
-                  </div>            
-                </div>
-                <div id="button_container">
-                  <div className="viewing_button_container">
-                    <div id="edit_button_container">
-                      <button 
-                        onClick={newTaskSaveDiscardHandle}
-                      >Cancel</button>
-                    </div>
-                    <div id="remove_button_container">
-                      <button 
-                        onClick={addTemplateTask}
-                      >Save</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+
             <div>
               {
-                templateTasks.map((task) => {
+                templateTasks?.map((task) => {
                   return <TaskTemplate 
                   key={task.id}
                   task={task}
@@ -175,13 +191,15 @@ export default function TasksTemplatesBuyPage(){
                 })
               }
             </div>
+
           </div>
         </div>
-        <div id="right_side_container">
-          <div id="cal_container"></div>
-          <div id="tasks_container"></div>
+        <div className="col-lg-4 d-none d-lg-block">
+          <Calendar/>
+          <AllTasks/>
         </div>
-      </div> 
+      </div>
+    </div>
     </>
   )
 }
